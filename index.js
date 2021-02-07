da9511fc0da97b61fa8d0b5e85584ff172d70974
Gabriel Nardes <gabrielnardeslobao@gmail.com>
2021-02-06 21:05:34 -0300

    Fix bug in setBarHeight()

--- index.js
+++ index.js
@@ -1,80 +1,30 @@
 var g_array = [];
 var random_array = [];
 var delay_ms = 10;
+var old_height = 55;
 
-function mySpan(id, height, div, i)
+function createRandomArray()
 {
-    let span = document.createElement("span");
-    span.id = id + i;
-    span.classList.add("bar");
-    span.style.height = height + "px";
-    span.style.width = $("bar_width_slider").value + "px";
-    $(div).appendChild(span);
+    for (let i = 0; i < $("bar_qty_slider").max; i++)
+        random_array[i] = genRandomNumber(5, 300); // height min and max
 }
 
 function createBars()
 {
-    $("bot").textContent = '';  // clean array
-
-    let gen_array = [];
+    //let gen_array = [];
+    // CHANGE THIS SLIDER
     for (let i = 0; i < $("bar_qty_slider").value; i++)
     {
-        //gen_array[i] = genRandomNumber(5, $("bar_height_slider").value);
-        random_array[i] = genRandomNumber(5, 300);
-        gen_array[i] =  random_array[i] * $("bar_height_slider").value / 100;
-
-        mySpan("span", gen_array[i], "bot", i);
-        mySpan("span_selection_u", gen_array[i], "selection_u", i);
-        mySpan("span_selection_s", gen_array[i], "selection_s", i);
-        mySpan("span_bubble", gen_array[i], "bubble", i);
+        let random =  random_array[i] * $("bar_height_slider").value / 100;
+        mySpan("span", random, "bot", i);
+        mySpan("span_selection_u", random, "selection_u", i);
+        mySpan("span_selection_s", random, "selection_s", i);
+        mySpan("span_bubble", random, "bubble", i);
     }
 
-    g_array = [...gen_array];
-}
-
-function setBarQty()
-{
-    let slider = $("bar_qty_slider");
-    let output = $("bar_qty_value");
-    output.innerHTML = slider.value;
-    genRandomArray();
-}
-
-function setBarHeight()
-{
-    let gen_array = [];
+    //g_array = [...gen_array];
 
     $("bar_height_value").innerHTML = $("bar_height_slider").value;
-
-    $('bot').textContent = '';
-    $('selection_u').textContent = '';
-    $('selection_s').textContent = '';
-    $('bubble').textContent = '';
-
-    for (let i = 0; i < $("bar_qty_slider").value; i++)
-    {
-        g_array[i] =  random_array[i] * $("bar_height_slider").value / 100;
-
-        mySpan("span", g_array[i], "bot", i);
-        mySpan("span_selection_u", g_array[i], "selection_u", i);
-        mySpan("span_selection_s", g_array[i], "selection_s", i);
-        mySpan("span_bubble", g_array[i], "bubble", i);
-    }
-}
-
-function setBarWidth()
-{
-    $("bar_width_value").innerHTML = $("bar_width_slider").value;
-
-    let width = $("bar_width_slider").value + "px";
-
-    for (let i = 0; i < $("bar_qty_slider").value; i++)
-    {
-        $("span" + i).style.width = width;
-        $("span_selection_u" + i).style.width = width;
-        $("span_selection_s" + i).style.width = width;
-        $("span_bubble" + i).style.width = width;
-    }
 }
 
 function genRandomArray()
@@ -107,6 +57,60 @@ function genRandomArray()
     g_array = [...gen_array];
 }
 
+function mySpan(id, height, div, i)
+{
+    let span = document.createElement("span");
+    span.id = id + i;
+    span.classList.add("bar");
+    span.style.height = height + "px";
+    span.style.width = $("bar_width_slider").value + "px";
+    $(div).appendChild(span);
+}
+
+function setBarQty()
+{
+    let slider = $("bar_qty_slider");
+    let output = $("bar_qty_value");
+    output.innerHTML = slider.value;
+    genRandomArray();
+}
+
+function setBarHeight()
+{
+    $("bar_height_value").innerHTML = $("bar_height_slider").value;
+
+    for (let i = 0; i < $("bar_qty_slider").value; i++)
+    {       
+        scale("span", i)
+        scale("span_selection_u", i)
+        scale("span_selection_s", i)
+        scale("span_bubble", i)
+    }
+    
+    old_height = $("bar_height_slider").value;
+}
+
+function scale(span, i)
+{
+    let original = parseFloat($(span + i).style.height) * 100 / old_height;
+    $(span + i).style.height = original * $("bar_height_slider").value / 100 + "px";
+}
+
+function setBarWidth()
+{
+    $("bar_width_value").innerHTML = $("bar_width_slider").value;
+
+    let width = $("bar_width_slider").value + "px";
+
+    for (let i = 0; i < $("bar_qty_slider").value; i++)
+    {
+        $("span" + i).style.width = width;
+        $("span_selection_u" + i).style.width = width;
+        $("span_selection_s" + i).style.width = width;
+        $("span_bubble" + i).style.width = width;
+    }
+}
+
 function createSpan(id, array, parent)
 {
     for (let i = 0; i < array.length; i++)
@@ -120,51 +124,54 @@ function createSpan(id, array, parent)
     }
 }
 
+function reset(id)
+{
+    for (let i = 0; i < $("bar_qty_slider").value; i++)
+    {
+        $(id + i).style.height = random_array[i] * $("bar_height_slider").value / 100 + "px";
+    }
+}
+
 async function selectionSortUnstable()
 {
-    let array = [...g_array];
+    let array = [...random_array];
     let id = "span_selection_u";
 
-    $("selection_u").textContent = '';
+    reset(id);
 
-    createSpan(id, array, $('selection_u'));
-
-    for (let i = 0; i < array.length; i++)
+    for (let i = 0; i < $("bar_qty_slider").value; i++)
     {
         await sleep(delay_ms);
 
         let min_i = i;
-        for (let j = i + 1; j < array.length; j++)
+        for (let j = i + 1; j < $("bar_qty_slider").value; j++)
             if (array[j] < array[min_i])
                 min_i = j;
 
         // swap elements
         let temp = array[min_i];
+
         array[min_i] = array[i];
+        $(id + min_i).style.height = array[i] * $("bar_height_slider").value / 100 + "px";
+
         array[i] = temp;
-
-
-        let temp_heigth = $(id + min_i).style.height;
-        $(id + min_i).style.height = $(id + i).style.height;
-        $(id + i).style.height = temp_heigth;
+        $(id + i).style.height = temp * $("bar_height_slider").value / 100 + "px";
     }
 }
 
 async function selectionSortStable()
 {
-    let array = [...g_array];
+    let array = [...random_array];
     let id = "span_selection_s";
 
-    $('selection_s').textContent = '';
-
-    createSpan(id, array, $('selection_s'));
-
-    for (let i = 0; i < array.length; i++)
+    reset(id);
+    
+    for (let i = 0; i < $("bar_qty_slider").value; i++)
     {
         await sleep(delay_ms);
 
         let min_i = i;
-        for (let j = i + 1; j < array.length; j++)
+        for (let j = i + 1; j < $("bar_qty_slider").value; j++)
             if (array[j] < array[min_i])
                 min_i = j;
 
@@ -174,25 +181,23 @@ async function selectionSortStable()
         for (let m = min_i; m > i; m--)
         {
             array[m] = array[m-1];
-            $(id + m).style.height = $(id + (m - 1)).style.height;
+            $(id + m).style.height = array[m-1] * $("bar_height_slider").value / 100 + "px";
 
             array[m-1] = min;
-            $(id + (m - 1)).style.height = min_heigth;
+            $(id + (m - 1)).style.height = min * $("bar_height_slider").value / 100 + "px";
         }
 
         array[i] = min;
-        $(id + i).style.height = min_heigth;
+        $(id + i).style.height = min * $("bar_height_slider").value / 100 + "px";
     }
 }
 
 async function bubbleSort()
 {
-    let array = [...g_array];
+    let array = [...random_array];
     let id = "span_bubble";
 
-    $('bubble').textContent = '';
-
-    createSpan(id, array, $('bubble'));
+    reset(id);
 
     let sort_incomplete = true;
     while (sort_incomplete)
@@ -200,18 +205,17 @@ async function bubbleSort()
         await sleep(delay_ms);
         
         sort_incomplete = false;
-        for (let i = 0; i < array.length; i++)
+        for (let i = 0; i < $("bar_qty_slider").value - 1; i++)
         {
             if (array[i + 1] < array[i])
             {
                 let temp = array[i];
-                let temp_bar = $(id + i).style.height;
 
                 array[i] = array[i + 1];
-                $(id + i).style.height = $(id + (i + 1)).style.height;
+                $(id + i).style.height = array[i + 1] * $("bar_height_slider").value / 100 + "px";
 
                 array[i + 1] = temp;
-                $(id + (i + 1)).style.height = temp_bar;
+                $(id + (i + 1)).style.height = temp * $("bar_height_slider").value / 100 + "px";
 
                 sort_incomplete = true;
             }
